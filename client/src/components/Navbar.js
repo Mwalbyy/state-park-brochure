e830178c91429a5b00c7cdfe25de4674342b99c3
Marcus Walby <marcuswalby24@gmail.com>
2022-04-22 23:31:14 -0400

    added navbar and page links

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function Navbar() {
+export default function Navigation({ currentPage, handlePageChange }) {
   return (
-    <div>NavBar</div>
-  )
+    <>
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+        className="fixedPos"
+      >
+        <Container>
+          <Navbar.Brand href="#home"
+                onClick={() => handlePageChange("Home")}
+                className={
+                  currentPage === "Home"
+                    ? "nav-link active navText"
+                    : "nav-link navText"
+                }>
+            <div>National Park </div>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                href="#home"
+                onClick={() => handlePageChange("Home")}
+                className={
+                  currentPage === "Home"
+                    ? "nav-link active navText"
+                    : "nav-link navText"
+                }
+              >
+                Home
+              </Nav.Link>
+              <Nav.Link
+                href="#Posts"
+                onClick={() => handlePageChange("Posts")}
+                className={
+                  currentPage === "Posts"
+                    ? "nav-link active navText"
+                    : "nav-link navText"
+                }
+              >
+                Posts
+              </Nav.Link>
+              <Nav.Link
+                href="#Weather"
+                onClick={() => handlePageChange("Weather")}
+                className={
+                  currentPage === "Weather"
+                    ? "nav-link active navText"
+                    : "nav-link navText"
+                }
+              >
+                Weather
+              </Nav.Link>
+              <Nav.Link
+                href="#Profile"
+                onClick={() => handlePageChange("Profile")}
+                className={
+                  currentPage === "Profile"
+                    ? "nav-link active navText"
+                    : "nav-link navText"
+                }
+              >
+                Profile
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
