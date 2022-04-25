55c269fc6f38f4ba0aaec9a1544aa910c0839fac
Lyere <BPpearsall@gmail.com>
2022-04-25 10:26:53 -0400

    Created first draft of typeDefs, waiting on custom image logic, and added an auth.js files to Utils to handle login logic

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,52 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql `
-    
+    type User {
+        _id: ID
+        username: String
+        email: String
+        password: String
+        posts: [Post]!
+    }
+
+    type Post {
+        _id: ID
+        postText: String
+        postImage: String
+        createdAt: String
+        comments: [Comment]!
+    }
+
+    type Comment {
+        _id: ID
+        commentText: String
+        createdAt: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        user(username: String): [Thought]
+        user(username: String): [Comment]
+        thought(thoughtId: ID!): Thought
+        comment(commentId: ID!): Comment
+    }
+
+    type Mutation {
+        addUser(username: String!, email: String!, password: String!): Auth
+        login(email: String!, password: String!): Auth
+        addPost(postText: String!, postImage: String!): Thought
+        addComment(
+            postId: ID!
+            commentText: String!
+        ): Post
+        removePost(postId: ID!): Post
+        removeComment(postId: ID!, commentId ID!): Thought 
+    }
 `
 
 module.exports = typeDefs;
