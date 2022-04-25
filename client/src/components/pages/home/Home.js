d37ee84f05ee953efd4ff8131bddebb2677cd0c6
Marcus Walby <marcuswalby24@gmail.com>
2022-04-25 13:41:23 -0400

    create format for history section of paralex

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -4,12 +4,25 @@ import forest from "./images/njTest2.jpg";
 import pastPic from "./images/history1.jpg";
 
 export default function Home() {
-  const center = { display: 'flex', alignItems: 'center',justifyContent: 'center'}
-  const alignCenterRight ={display: 'flex', alignItems: 'center',justifyContent: 'flex-start'}
-  const alignCenterLeft ={display: 'flex', alignItems: 'center',justifyContent: 'flex-end'}
+  const center = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  };
+  const alignCenterRight = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-start",
+  };
+  const alignCenterLeft = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "flex-end",
+  };
   return (
     <>
       <Parallax pages={6}>
+        {/* Cover page */}
         <ParallaxLayer
           offset={0}
           speed={1}
@@ -23,41 +36,47 @@ export default function Home() {
             <h1 className="parkName F-3">New Jersey Cedar reserve </h1>
           </ParallaxLayer>
         </ParallaxLayer>
+        {/* backGround for the history section */}
         <ParallaxLayer
           speed={1}
           offset={1}
           factor={6}
           style={{ backgroundColor: "#162529" }}
         ></ParallaxLayer>
-        <ParallaxLayer speed={0.5} offset={1.1} sticky={{ start: 1.1, end: 3 }}>
+        {/* Title of history section */}
+        <ParallaxLayer speed={0.5} offset={1.1} sticky={{ start: 1.1, end: 3.5 }}>
           <h1 className="heading F-3"> brief History</h1>
         </ParallaxLayer>
+        {/* single History card */}
         <ParallaxLayer
-        speed={2}
-        offset={2.3}
-        factor={3}
-        sticky={{ start: 1.3, end: 1.8 }}
-        style={{...center}}
+          speed={2}
+          offset={2.3}
+          factor={3}
+          sticky={{ start: 1.3, end: 2 }}
+          style={{ ...center }}
         >
-          <h1 className='cardText F-1 '>SUP</h1>
+          <h1 className="cardText F-1 ">SUP</h1>
+        </ParallaxLayer>
+        {/* Double History cards */}
+        <ParallaxLayer
+          speed={2}
+          offset={2.3}
+          factor={3}
+          sticky={{ start: 2.3, end: 3.5 }}
+          style={{ ...alignCenterLeft }}
+        >
+          <h1 className="cardText F-1 ">SUP</h1>
         </ParallaxLayer>
         <ParallaxLayer
-        speed={2}
-        offset={2.3}
-        factor={3}
-        sticky={{ start: 2, end: 3 }}
-        style={{...alignCenterLeft}}
+          speed={2}
+          offset={2.3}
+          factor={3}
+          sticky={{ start:  2.3, end: 3.5 }}
+          style={{ ...alignCenterRight }}
         >
-          <h1 className='cardText F-1 '>SUP</h1>
-        </ParallaxLayer>
-        <ParallaxLayer
-        speed={2}
-        offset={2.3}
-        factor={3}
-        sticky={{ start: 2, end: 3 }}
-        style={{...alignCenterRight}}
-        >
-          <h1 className='cardText F-1 '>SUP</h1>
+          <h1 className=" ">
+            <img src={pastPic} alt="BigCo Inc. logo" />
+          </h1>
         </ParallaxLayer>
       </Parallax>
     </>
