6620e79c92baa74012124a8ff9d24a710c4d7761
Lyere <BPpearsall@gmail.com>
2022-04-28 13:13:16 -0400

    Working profile page with displayed posts unique to user

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -18,17 +18,23 @@ query getAllPosts {
 `;
 
 export const QUERY_USER = gql`
-query getUser($username: String) {
+query user($username: String) {
   user(username: $username) {
     _id
     username
     email
     password
+    posts {
+      _id
+      postText
+      postAuthor
+      createdAt
+    }
   }
 }`
 
 export const QUERY_ME = gql`
-query Query {
+query me {
   me {
     _id
     username
