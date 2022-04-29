908c0f74cb6436c47335575cf64b6ec6fd3fa5e9
Lyere <BPpearsall@gmail.com>
2022-04-29 14:17:40 -0400

    removed incorrect await in resolvers.js for users

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -27,7 +27,7 @@ const resolvers = {
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
-      return await { token, user };
+      return { token, user };
     },
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
