84cfdd09ae54f890849d7aaff5641efed101cbb8
Marcus Walby <marcuswalby24@gmail.com>
2022-05-01 12:25:22 -0400

    turned comments into cards

--- client/src/components/CommentList.js
+++ client/src/components/CommentList.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React from "react";
+import Card from "react-bootstrap/Card";
 
+function CommentList({ comments }) {
+  return (
+    <div className="commentBox pt-3">
+      {comments &&
+        comments.map((comment) => (
+          <>
+            <div key={comment._id}>
+            </div>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title>{comment.commentAuthor}</Card.Title>
+                <Card.Text>
+                {comment.commentText}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </>
+        ))}
+    </div>
+  );
+}
 
-function CommentList({comments, commentAuthor, commentText}) {
-    return (
-      <div className="commentBox pt-3">\
-      {comments && 
-      comments.map((comment) => (
-        <div key={comment._id}>
-        <h1>{comment.commentText}</h1>
-        </div>
-      ))}
-      </div>
-    );
-  }
-  
-  export default CommentList;
+export default CommentList;
